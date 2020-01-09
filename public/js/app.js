1d8f2c9c666077a79f0d1b62c40ca6ecb3fc59dd
Emaneliforp <kietchouam@gmail.com>
2020-01-09 15:22:31 -0800

    change firebase

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
-let name = document.getElementById("name");
+let db = firebase.database();
+let auth = firebase.auth();
+let nameIn = document.getElementById("name");
 let submitBtn = document.getElementById("submitBtn");
+let userId;
+
+firebase.auth().onAuthStateChanged(function(user) { //waits until current user is fully initialized, before trying to capture user ID
+    if (user) { //tests to make sure current user is not null
+    console.log("user successful!!");
+    userId=firebase.auth().currentUser.uid; //get the current user's id
+    console.log(userId);
+    }
+})
 
 submitBtn.addEventListener('click',function(){save()});
 
 function save(){
-    
+    db.ref(`users/${userId}`).set({
+        name: prompt("Yo")
+      })
 }
