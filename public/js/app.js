debd2e436bf91fa3e4e65f1d420becae9377ce75
Kyle Dang <justsogaming253@gmail.com>
2020-02-11 16:29:32 -0800

    backend database for all questions complete

--- public/js/app.js
+++ public/js/app.js
@@ -10,21 +10,22 @@ let curYear = d.getFullYear(); //get current year (first branch)
 function save(){
     console.log("hi");
     let formSubmission={}; //final object with all questions 
-    let section1=document.getElementById("1"); //all info saved under name branch
+    let section1=document.getElementById("1");
     let section; 
-    sendData(); //check each field and send to DB
 
-    
-    function sendData(){
+    let submitToFirebase=(()=>{ 
         for(let i = 1; i<11; i++){ //question sections
             section=document.getElementById(`${i}`) 
             for(let elementNum=0; elementNum<section.length; elementNum++){ //questions in each section
-                if(section[elementNum].value!=""){ //verify each question is filled
+                if(section.elements[elementNum].type==="text" && section[elementNum].value!=""){ //verify only answer is being retrieved and filled in 
+                    console.log(`${i}_${elementNum}`)
+                    formSubmission[`${i}_${elementNum}_Q: `]=document.getElementById(`${i}_${elementNum}`).innerHTML; //questions, retrieve using their id in the p tags before the answer
                     formSubmission[`${i}_${elementNum}`]=section[elementNum].value; //object to be pushed
                 }
             }
         }
-    }
+    });
+    submitToFirebase(); 
     console.log(formSubmission)
     db.ref(`applications/${curYear}/${section1.elements[0].value}`).set(formSubmission)
 }
